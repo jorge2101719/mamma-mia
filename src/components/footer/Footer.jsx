7fccd3f8f1df02ccd492dafd1e8a2a16be2515d8
jorge2101719 <rusosky01@gmail.com>
2023-10-21 15:37:51 -0300

    modificación en Footer para dar efecto de ocultar/mostrar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,9 +10,12 @@ const Footer = () => {
   return (
   <div className="bg-dark text-light p-3">
     <p className='fs-3 fw-bold'>Pizzería Mamma Mia</p>
-    <p className='fs-5 fw-bold'>El total de su compra es: ${total.toLocaleString('cl-CL')}</p>
-    <Button variant="success" onClick={() => pagar()}>Pagar</Button>
-    <Button onClick={() => limpiarElCarrito()}>Limpiar el carrito</Button>
+    {total > 0 ? (
+    <div>
+      <p className='fs-5 fw-bold'>El total de su compra es: ${total.toLocaleString('cl-CL')}</p>
+      <Button variant="success" onClick={() => pagar()}>Pagar</Button>
+      <Button onClick={() => limpiarElCarrito()}>Limpiar el carrito</Button>
+    </div>) : ('')}
     <Form className='p-2'>
       <Form.Group className='m-2'>
         <Form.Label>Déjenos su comentario</Form.Label>
